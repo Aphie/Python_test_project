68e67686144601291906249833d27209e643a39b
s.andreyuk <s.andreyuk@crpt.ru>
2023-04-27 18:49:55 +0300

    add going to login page

--- test_main_page.py
+++ test_main_page.py
@@ -9,9 +9,6 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.should_be_login_link()      # выполняем метод страницы — ищем ссылку на логин
-    
-def test_guest_can_view_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_page()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
